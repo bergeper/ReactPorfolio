a0bbc66f67be10e9bc0f708362f187c94ea1d581
Per Berge <per.berge93@gmail.com>
2023-05-17 22:05:17 +0200

    (feat): added classNames

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,29 @@
 import { About } from '../About/About';
 import { Contact } from '../Contact/Contact';
-import { Navbar } from '../Navigation/Navbar';
+import { Hero } from '../Hero/Hero';
+import { Navigation } from '../Navigation/Navigation';
 import { Projects } from '../Projects/Projects';
+import './Layout.scss';
 
 export const Layout = () => {
   return (
     <>
       <header>
-        <Navbar></Navbar>
+        <Navigation></Navigation>
+        <Hero></Hero>
       </header>
-      <main>
-        <About></About>
-        <Projects></Projects>
-        <Contact></Contact>
+      <main className='main--container'>
+        <section className='content'>
+          <Projects></Projects>
+        </section>
+        <section className='content'>
+          <About></About>
+        </section>
+        <section className='content'>
+          <Contact></Contact>
+        </section>
       </main>
-      <footer></footer>
+      <footer className='foot'>footer</footer>
     </>
   );
 };
