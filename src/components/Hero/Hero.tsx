c6e770d24fbd77617cbf08a8a249b644341c698f
Per Berge <per.berge93@gmail.com>
2023-05-18 22:12:45 +0200

    (feat): basic styling too see elements better

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,3 +1,12 @@
+import './Hero.scss';
+
 export const Hero = () => {
-  return <></>;
+  return (
+    <>
+      <section className='hero'>
+        <h1>Something fucking cool here</h1>
+        <section>Bottom nav</section>
+      </section>
+    </>
+  );
 };
