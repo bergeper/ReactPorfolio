0d017282d959ba5c23d11e6c268ad89f8aff2732
Per Berge <per.berge93@gmail.com>
2023-05-18 22:12:53 +0200

    (feat): menu added

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,26 +1,56 @@
+import { useState } from 'react';
 import './Navigation.scss';
 
 export const Navigation = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const showMenu = () => {
+    if (toggleMenu === true) {
+      setMenuOpen(false);
+      setToggleMenu(false);
+    } else {
+      setMenuOpen(true);
+      setToggleMenu(true);
+    }
+  };
+
   return (
-    <nav className='navbar'>
-      <h3 className='navbar__title'>P.Berge</h3>
-      <ul className='navbar__menu'>
-        <li className='navbar__menu--item'>
-          <a className='navbar__menu--link' href='#projects'>
-            Projects
-          </a>
-        </li>
-        <li className='navbar__menu--item'>
-          <a className='navbar__menu--link' href='#projects'>
-            About
-          </a>
-        </li>
-        <li className='navbar__menu--item'>
-          <a className='navbar__menu--link' href='#projects'>
-            Contact
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <button
+        onClick={showMenu}
+        className={menuOpen ? 'open--menu' : 'closed--menu'}
+      >
+        {menuOpen ? 'Close Menu' : 'Open Menu'}
+      </button>
+      <nav className='menu'>
+        {toggleMenu ? (
+          <ul className='menu__list'>
+            <li className='menu__list--item'>
+              <a className='menu__list--link' href='#hero'>
+                Home
+              </a>
+            </li>
+            <li className='menu__list--item'>
+              <a className='menu__list--link' href='#projects'>
+                Projects
+              </a>
+            </li>
+            <li className='menu__list--item'>
+              <a className='menu__list--link' href='#about'>
+                About
+              </a>
+            </li>
+            <li className='menu__list--item'>
+              <a className='menu__list--link' href='#contact'>
+                Contact
+              </a>
+            </li>
+          </ul>
+        ) : (
+          ' '
+        )}
+      </nav>
+    </>
   );
 };
