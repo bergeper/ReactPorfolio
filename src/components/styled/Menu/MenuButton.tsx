b91f17e57d83deb5f73118b87af055c4b867078a
Per Berge <per.berge93@gmail.com>
2023-05-24 21:51:57 +0200

    (feat): styled-component for Menu Button

---
+++ src/components/styled/Menu/MenuButton.tsx
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components';
+
+export const MenuButton = styled.button`
+  position: fixed;
+  padding: 8px;
+  margin: 5px;
+  font-size: 20px;
+  top: 10px;
+  right: 10px;
+  border: 3px solid white;
+  background-color: #849efcff;
+  &:hover {
+    background-color: #fded54ff;
+  }
+`;
