6af02c7e8d31d9cd8e66c1367f9e399f69c9a95d
Per Berge <per.berge93@gmail.com>
2023-05-24 21:51:13 +0200

    (feat): styled-component for nav

---
+++ src/components/styled/BottomNav.tsx
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const BottomNav = styled.nav`
+  @media screen and (min-width: 768px) {
+    height: 15vh;
+    background-color: #5deb5dd6;
+    display: flex;
+  }
+`;
