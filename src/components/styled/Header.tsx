f80605e79e988dfb7b40af63cda26db53e3756ba
Per Berge <per.berge93@gmail.com>
2023-05-24 21:51:30 +0200

    (feat): styled-component for header

---
+++ src/components/styled/Header.tsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+export const Header = styled.section`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #1bbd1bbb;
+  @media screen and (min-width: 768px) {
+    height: 85vh;
+  }
+`;
