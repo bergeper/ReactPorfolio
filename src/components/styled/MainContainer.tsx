4accba66d2bda7b3e0c7ecbb90e688e1359c579b
Per Berge <per.berge93@gmail.com>
2023-05-24 21:51:44 +0200

    (feat): styled-component for main container

---
+++ src/components/styled/MainContainer.tsx
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const MainContainer = styled.main`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: #e8e2a9;
+`;
