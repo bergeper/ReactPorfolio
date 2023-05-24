e7ba00590ea5408d93b260ed07907661af624cee
Per Berge <per.berge93@gmail.com>
2023-05-24 21:52:04 +0200

    (feat): styled-component for Menu item

---
+++ src/components/styled/Menu/MenuItem.tsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+export const MenuItem = styled.li`
+  border: 3px solid white;
+  background-color: #849efcff;
+  padding: 5px;
+  margin: 5px;
+  list-style-type: none;
+  &:hover {
+    background-color: #fded54ff;
+  }
+`;
