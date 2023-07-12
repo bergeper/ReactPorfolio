0bb8c7435555d74831315fad2f56edbef877a9e5
Per Berge <per.berge93@gmail.com>
2023-07-12 22:26:00 +0200

    (feat): added animation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react';
-import { MenuButton } from '../styled/Menu/MenuButton';
-import { Menu } from '../styled/Menu/Menu';
-import { MenuList } from '../styled/Menu/MenuList';
-import { MenuItem } from '../styled/Menu/MenuItem';
-import { MenuLink } from '../styled/Menu/MenuLink';
-import { MenuIcon } from '../styled/Menu/MenuIcon';
-import projects from '../../assets/images/logo/projects4.png';
-import home from '../../assets//images/logo/home2.png';
-import contact from '../../assets/images/logo/contact.png';
-import about from '../../assets/images/logo/about.png';
+import { useState } from "react";
+import { MenuButton } from "../styled/Menu/MenuButton";
+import { Menu } from "../styled/Menu/Menu";
+import { MenuList } from "../styled/Menu/MenuList";
+import { MenuItem } from "../styled/Menu/MenuItem";
+import { MenuLink } from "../styled/Menu/MenuLink";
+import { MenuIcon } from "../styled/Menu/MenuIcon";
+import projects from "../../assets/images/logobw/projects.png";
+import home from "../../assets//images/logobw/home.png";
+import contact from "../../assets/images/logobw/contact.png";
+import about from "../../assets/images/logobw/about.png";
+import { Slide } from "react-awesome-reveal";
 
 export const Navigation = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -27,38 +28,40 @@ export const Navigation = () => {
   return (
     <>
       <MenuButton onClick={showMenu}>
-        {menuOpen ? 'Close Menu' : 'Menu'}
+        {menuOpen ? "Close Menu" : "Menu"}
       </MenuButton>
       <Menu>
         {toggleMenu ? (
-          <MenuList>
-            <MenuItem>
-              <MenuLink href='#hero' onClick={showMenu}>
-                <MenuIcon src={home}></MenuIcon>
-                Home
-              </MenuLink>
-            </MenuItem>
-            <MenuItem>
-              <MenuLink href='#projects' onClick={showMenu}>
-                <MenuIcon src={projects}></MenuIcon>
-                Projects
-              </MenuLink>
-            </MenuItem>
-            <MenuItem>
-              <MenuLink href='#about' onClick={showMenu}>
-                <MenuIcon src={about}></MenuIcon>
-                About
-              </MenuLink>
-            </MenuItem>
-            <MenuItem>
-              <MenuLink href='#contact' onClick={showMenu}>
-                <MenuIcon src={contact}></MenuIcon>
-                Contact
-              </MenuLink>
-            </MenuItem>
-          </MenuList>
+          <Slide direction="right">
+            <MenuList>
+              <MenuItem>
+                <MenuLink href="#hero" onClick={showMenu}>
+                  <MenuIcon src={home}></MenuIcon>
+                  Home
+                </MenuLink>
+              </MenuItem>
+              <MenuItem>
+                <MenuLink href="#projects" onClick={showMenu}>
+                  <MenuIcon src={projects}></MenuIcon>
+                  Projects
+                </MenuLink>
+              </MenuItem>
+              <MenuItem>
+                <MenuLink href="#about" onClick={showMenu}>
+                  <MenuIcon src={about}></MenuIcon>
+                  About
+                </MenuLink>
+              </MenuItem>
+              <MenuItem>
+                <MenuLink href="#contact" onClick={showMenu}>
+                  <MenuIcon src={contact}></MenuIcon>
+                  Contact
+                </MenuLink>
+              </MenuItem>
+            </MenuList>
+          </Slide>
         ) : (
-          ' '
+          " "
         )}
       </Menu>
     </>
