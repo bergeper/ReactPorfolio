402c64f1d5d232772cc2d0ee676b835caa83a81c
Per Berge <per.berge93@gmail.com>
2023-07-12 22:25:49 +0200

    (feat): added animation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,24 +1,17 @@
-//import logo from "../../assets/CompPurp.png";
+import { Slide } from "react-awesome-reveal";
 import { HeroDesc } from "../styled/Hero/HeroDesc";
-//import { HeroImg } from "../styled/Hero/HeroImg";
-//import { HeroImgContainer } from "../styled/Hero/HeroImgContainer";
-import { HeroImgSection } from "../styled/Hero/HeroImgSection";
 import { HeroTitle } from "../styled/Hero/HeroTitle";
 import { HeroTitleSection } from "../styled/Hero/HeroTitleSection";
 export const Hero = () => {
-  /*
-  <HeroImgContainer>
-    <HeroImg src={logo}></HeroImg>
-  </HeroImgContainer>
-  */
   return (
     <>
       <HeroTitleSection>
-        <HeroTitle>Hi, I am</HeroTitle>
-        <HeroTitle>Per</HeroTitle>
-        <HeroDesc>Front-End Developer</HeroDesc>
+        <Slide cascade>
+          <HeroTitle>Hi, I am</HeroTitle>
+          <HeroTitle>Per</HeroTitle>
+          <HeroDesc>Soon To Be Front-End Developer</HeroDesc>
+        </Slide>
       </HeroTitleSection>
-      <HeroImgSection></HeroImgSection>
     </>
   );
 };
