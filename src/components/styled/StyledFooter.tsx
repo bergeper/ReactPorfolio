ef107a05f69f4e59c943dedbce77268db7a4e597
Per Berge <per.berge93@gmail.com>
2023-08-14 18:17:57 +0200

    nan

--- src/components/styled/StyledFooter.tsx
+++ src/components/styled/StyledFooter.tsx
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
   width: 100%;
   height: 150px;
-  background-color: gold;
+  background-color: #777762;
 `;
