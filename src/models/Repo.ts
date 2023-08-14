f308e690f1039dcafcaf4c0a6f69147b04bc0bf6
Per Berge <per.berge93@gmail.com>
2023-08-14 20:55:41 +0200

    (feat): class for project

---
+++ src/models/Repo.ts
@@ -0,0 +1,10 @@
+export class Repo {
+  constructor(
+    public name: string,
+    public imgLink: string,
+    public imgMobile: string,
+    public imgDesktop: string,
+    public text: string,
+    public github: string
+  ) {}
+}
