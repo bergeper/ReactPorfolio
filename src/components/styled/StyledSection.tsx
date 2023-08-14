515ff88a53ac5438c586f79f3d02adf0da0cee35
Per Berge <per.berge93@gmail.com>
2023-08-14 18:18:13 +0200

    (refactor): fixed pos

--- src/components/styled/StyledSection.tsx
+++ src/components/styled/StyledSection.tsx
@@ -1,8 +1,9 @@
-import { styled } from 'styled-components';
+import { styled } from "styled-components";
 
 export const StyledSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
+  position: relative;
   min-height: 100vh;
 `;
