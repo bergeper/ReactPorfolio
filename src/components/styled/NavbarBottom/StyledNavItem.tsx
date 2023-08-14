f3283871572051525d384ba8dcc11f03e9575504
Per Berge <per.berge93@gmail.com>
2023-08-14 18:17:50 +0200

    (fix): will display on top

--- src/components/styled/NavbarBottom/StyledNavItem.tsx
+++ src/components/styled/NavbarBottom/StyledNavItem.tsx
@@ -4,6 +4,7 @@ export const StyledNavItem = styled.li`
   display: flex;
   justify-content: center;
   align-items: center;
+  z-index: 10;
   @media screen and (min-width: 768px) {
     list-style: none;
   }
