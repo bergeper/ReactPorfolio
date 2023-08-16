ec7aedd89d7afd78151721dd1e3b03ca2f5ec526
Per Berge <per.berge93@gmail.com>
2023-08-16 14:45:33 +0200

    (feat): startpage

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,31 +1,26 @@
-import { Slide } from "react-awesome-reveal";
-import { HeroDesc } from "../../styled/Hero/HeroDesc";
-import { HeroDescContainer } from "../../styled/Hero/HeroDescContainer";
-import { HeroTitle } from "../../styled/Hero/HeroTitle";
-import { HeroTitleSection } from "../../styled/Hero/HeroTitleSection";
-import { BottomNavigation } from "../../navigation/BottomNavigation";
-import { StyledSemiCircle } from "../../styled/Figures/StyledSemiCircle";
-import { StyledHeader } from "../../styled/StyledHeader";
+import { StyledContainer } from "../../styled/Content/StyledContainer";
+import { StyledContent } from "../../styled/Content/StyledContent";
+import { StyledPageHeading } from "../../styled/StyledPageHeading";
+import { StyledSection } from "../../styled/StyledSection";
 
 export const Home = () => {
   return (
     <>
-      <StyledHeader>
-        <HeroTitleSection>
-          <Slide>
-            <HeroDescContainer>
-              <Slide cascade>
-                <HeroTitle>Hi, I am</HeroTitle>
-                <HeroTitle>Per</HeroTitle>
-                <HeroDesc>Soon To Be</HeroDesc>
-                <HeroDesc>Front-End Developer</HeroDesc>
-              </Slide>
-            </HeroDescContainer>
-          </Slide>
-        </HeroTitleSection>
-        <StyledSemiCircle></StyledSemiCircle>
-      </StyledHeader>
-      <BottomNavigation></BottomNavigation>
+      <StyledSection>
+        <StyledPageHeading id="test">Well this fking works</StyledPageHeading>
+        <StyledContainer>
+          <StyledContent>Projects</StyledContent>
+          <StyledContent>Projects</StyledContent>
+        </StyledContainer>
+        <StyledContainer>
+          <StyledContent>About</StyledContent>
+          <StyledContent>About</StyledContent>
+        </StyledContainer>
+        <StyledContainer>
+          <StyledContent>Contact</StyledContent>
+          <StyledContent>Contact</StyledContent>
+        </StyledContainer>
+      </StyledSection>
     </>
   );
 };
