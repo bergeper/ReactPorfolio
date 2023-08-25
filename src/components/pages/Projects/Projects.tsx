7b1d7d6e298b79750dd6397cccd82e50942e2d4f
Per Berge <per.berge93@gmail.com>
2023-08-25 19:55:16 +0200

    (refactor): spacing between link and text

--- src/components/pages/Projects/Projects.tsx
+++ src/components/pages/Projects/Projects.tsx
@@ -10,10 +10,12 @@ import { StyledPageHeadingDiv } from "../../styled/StyledPageHeadingDiv";
 import { StyledDesc } from "../../styled/Projects/StyledDesc";
 import { StyledPageHeadingSection } from "../../styled/StyledPageHeadingSection";
 import { topToScreen } from "../../../helpers/topToScreen";
+import { StyledProjectLink } from "../../styled/Project/StyledProjectLink";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export const Projects = () => {
   const [projects, setProjects] = useState<Repo[]>(data);
-
   const html = projects.map((pro, index) => (
     <Link to={"" + pro.id} key={index} onClick={topToScreen}>
       <StyledImgContainer>
@@ -30,8 +32,16 @@ export const Projects = () => {
         </StyledPageHeadingDiv>
       </StyledPageHeadingSection>
       <StyledDesc>
-        This is some of my school assignement projects. To check out other
-        projects, please visit my Github!
+        This is some of my school assignements. To check out other projects,
+        please visit my&nbsp;
+        <StyledProjectLink
+          href="https://github.com/bergeper/"
+          className="linkToPage"
+        >
+          <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+          Github!
+          <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
+        </StyledProjectLink>
       </StyledDesc>
       <StyledContainer>{html}</StyledContainer>
     </>
