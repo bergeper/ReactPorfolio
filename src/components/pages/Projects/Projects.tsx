faf1b5ba482872340089126affda508a27d9e530
Per Berge <per.berge93@gmail.com>
2023-08-25 17:47:23 +0200

    (feat): images will now be shown

--- src/components/pages/Projects/Projects.tsx
+++ src/components/pages/Projects/Projects.tsx
@@ -1,20 +1,39 @@
 import { Link } from "react-router-dom";
-import { StyledProjectContainer } from "../../styled/Projects/StyledProjectContainer";
+import { StyledContainer } from "../../styled/Projects/StyledContainer";
 import { StyledPageHeading } from "../../styled/StyledPageHeading";
-import { projects } from "../../../services/projects";
+import { data } from "../../../services/projects";
 import { StyledImgLink } from "../../styled/Projects/StyledImgLink";
+import { useState } from "react";
+import { Repo } from "../../../models/Repo";
+import { StyledImgContainer } from "../../styled/Projects/StyledImgContainer";
+import { StyledPageHeadingDiv } from "../../styled/StyledPageHeadingDiv";
+import { StyledDesc } from "../../styled/Projects/StyledDesc";
+import { StyledPageHeadingSection } from "../../styled/StyledPageHeadingSection";
+import { topToScreen } from "../../../helpers/topToScreen";
 
 export const Projects = () => {
-  const html = projects.map((pro) => (
-    <Link to={"" + pro.id}>
-      <StyledImgLink src={pro.imgLink}></StyledImgLink>
+  const [projects, setProjects] = useState<Repo[]>(data);
+
+  const html = projects.map((pro, index) => (
+    <Link to={"" + pro.id} key={index} onClick={topToScreen}>
+      <StyledImgContainer>
+        <StyledImgLink src={pro.imgLink}></StyledImgLink>
+      </StyledImgContainer>
     </Link>
   ));
 
   return (
     <>
-      <StyledPageHeading>Projects work</StyledPageHeading>
-      <StyledProjectContainer>{html}</StyledProjectContainer>
+      <StyledPageHeadingSection>
+        <StyledPageHeadingDiv>
+          <StyledPageHeading>Projects</StyledPageHeading>
+        </StyledPageHeadingDiv>
+      </StyledPageHeadingSection>
+      <StyledDesc>
+        This is some of my school assignement projects. To check out other
+        projects, please visit my Github!
+      </StyledDesc>
+      <StyledContainer>{html}</StyledContainer>
     </>
   );
 };
