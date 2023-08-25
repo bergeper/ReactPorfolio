858cdc77365fdb0dd7640a8a19a824eeeb6bf55c
Per Berge <per.berge93@gmail.com>
2023-08-25 18:04:10 +0200

    (feat): styled anchortag

---
+++ src/components/styled/Hero/HeroAnchor.tsx
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+export const HeroAnchor = styled.a`
+  text-decoration: none;
+  color: #fdeecd;
+  padding: 5px;
+  &:hover {
+    background-color: black;
+    border-radius: 9px;
+  }
+`;
